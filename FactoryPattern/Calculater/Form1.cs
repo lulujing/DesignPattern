6969d8288d2bf8da57ed25257952e2c08e2b60a8
jing lu <jinglu986@gmail.com>
2017-08-02 10:59:08 +1200

    AddFactoryPattern
    FactoryPattern

---
+++ FactoryPattern/Calculater/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Operation;     
+
+
+namespace Calculater
+{
+    public partial class Form1 : Form
+    {
+            bool bOperate = false;
+            Operation.Operation oper;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            if (bOperate)
+            {
+                txtShow.Text = "";
+                bOperate = false;
+            }
+
+            string number = ((Button)sender).Text;
+
+            txtShow.Text =Operation. Operation.checkNumberInput(txtShow.Text, number);
+
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            txtShow.Text = "";
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (txtShow.Text != "")
+            {
+                oper = OperationFactory.createOperate(((Button)sender).Text);
+
+                oper.NumberA = Convert.ToDouble(txtShow.Text);
+
+                bOperate = true;
+            }
+        }
+
+        private void buttonEqual_Click(object sender, EventArgs e)
+        {
+            if (txtShow.Text != "")
+            {
+                if (((Button)sender).Text != "=")
+                {
+                    oper = OperationFactory.createOperate(((Button)sender).Text);
+                }
+
+                oper.NumberB = Convert.ToDouble(txtShow.Text);
+
+
+                txtShow.Text = oper.GetResult().ToString();
+                bOperate = true;
+            }
+        }
+    }
+}
---
+++ FactoryPattern/Operation/Class1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operation
+{
+    /// <summary>
+    /// Operation Class
+    /// </summary>
+    public class Operation
+    {
+        private double _numberA = 0;
+        private double _numberB = 0;
+
+        /// <summary>
+        /// NumberA
+        /// </summary>
+        public double NumberA
+        {
+            get
+            {
+                return _numberA;
+            }
+            set
+            {
+                _numberA = value;
+            }
+        }
+
+        /// <summary>
+        /// NumberB
+        /// </summary>
+        public double NumberB
+        {
+            get
+            {
+                return _numberB;
+            }
+            set
+            {
+                _numberB = value;
+            }
+        }
+
+        /// <summary>
+        /// the result of Calculation
+        /// </summary>
+        /// <returns></returns>
+        public virtual double GetResult()
+        {
+            double result = 0;
+            return result;
+        }
+
+        /// <summary>
+        /// Vaildation
+        /// </summary>
+        /// <param name="currentNumber"></param>
+        /// <param name="inputString"></param>
+        /// <returns></returns>
+        public static string checkNumberInput(string currentNumber, string inputString)
+        {
+            string result = "";
+            if (inputString == ".")
+            {
+                if (currentNumber.IndexOf(".") < 0)
+                {
+                    if (currentNumber.Length == 0)
+                        result = "0" + inputString;
+                    else
+                        result = currentNumber + inputString;
+                }
+            }
+            else if (currentNumber == "0")
+            {
+                result = inputString;
+            }
+            else
+            {
+                result = currentNumber + inputString;
+            }
+
+            return result;
+        }
+
+
+    }
+
+    /// <summary>
+    /// Add Class
+    /// </summary>
+    class OperationAdd : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = NumberA + NumberB;
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// Subtraction
+    /// </summary>
+    class OperationSub : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = NumberA - NumberB;
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// multilplication
+    /// </summary>
+    class OperationMul : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = NumberA * NumberB;
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// division class
+    /// </summary>
+    class OperationDiv : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            if (NumberB == 0)
+                throw new Exception("除数不能为0。");
+            result = NumberA / NumberB;
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// 平方类
+    /// </summary>
+    class OperationSqr : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = NumberB * NumberB;
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// Square
+    /// </summary>
+    class OperationSqrt : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            if (NumberB < 0)
+                throw new Exception("负数不能开平方根。");
+            result = Math.Sqrt(NumberB);
+            return result;
+        }
+    }
+
+    /// <summary>
+    ///opposite Number
+    /// </summary>
+    class OperationReverse : Operation
+    {
+        public override double GetResult()
+        {
+            double result = 0;
+            result = -NumberB;
+            return result;
+        }
+    }
+
+    /// <summary>
+    ///selecting operation 
+    /// </summary>
+    public class OperationFactory
+    {
+        public static Operation createOperate(string operate)
+        {
+            Operation oper = null;
+            switch (operate)
+            {
+                case "+":
+                    {
+                        oper = new OperationAdd();
+                        break;
+                    }
+                case "-":
+                    {
+                        oper = new OperationSub();
+                        break;
+                    }
+                case "*":
+                    {
+                        oper = new OperationMul();
+                        break;
+                    }
+                case "/":
+                    {
+                        oper = new OperationDiv();
+                        break;
+                    }
+                case "sqr":
+                    {
+                        oper = new OperationSqr();
+                        break;
+                    }
+                case "sqrt":
+                    {
+                        oper = new OperationSqrt();
+                        break;
+                    }
+                case "+/-":
+                    {
+                        oper = new OperationReverse();
+                        break;
+                    }
+            }
+
+            return oper;
+        }
+    }
+}
